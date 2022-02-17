6fad3fd1b94e259af41fa989b5ddd8c120b2ccf0
IvanDamNation <ivan.suvalkin@gmail.com>
2022-02-17 21:54:05 +0300

    Add superuser and some new apps

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+
 ]
 
 SITE_ID = 1
